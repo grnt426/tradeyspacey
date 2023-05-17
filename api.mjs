111aa32424bd29d248e24582c0d6260dcbe4b21c
Grant Kurtz <th3guys2@gmail.com>
2023-05-17 19:52:31 -0400

    Basic server to return cached galaxy data at `/galaxy`.

--- api.mjs
+++ api.mjs
@@ -1,4 +1,20 @@
+import fs from "fs";
+
 const Client = require('node-rest-client').Client;
 const client = new Client();
 
-
+export default class API {
+    constructor(authTokenFile) {
+        this.AUTH_TOKEN_FILE = authTokenFile;
+        this.GAME_API = "https://api.spacetraders.io/v2/";
+        if(fs.existsSync(this.AUTH_TOKEN_FILE)) {
+            this.authToken = fs.readFileSync(this.AUTH_TOKEN_FILE, 'utf8');
+        }
+    }
+    
+    getBaseHeaders() {
+        return {
+            headers: {"Authorization": "Bearer " + this.authToken}
+        }
+    }
+}
---
+++ index.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import fs from "fs";
+
+const app = express();
+
+let galaxyData = {};
+
+app.get("/galaxy", (req, res) => {
+    res.status(200);
+    res.header("Content-Type", "application/json");
+    res.send(galaxyData);
+});
+
+const server = app.listen(8080, () => {
+    console.info("Server started");
+});
+
+const SYS_DATA_FILE = "systemdata.json"
+
+if(fs.existsSync(SYS_DATA_FILE)) {
+    galaxyData = JSON.parse(fs.readFileSync(SYS_DATA_FILE, 'utf8'));
+    console.info("Parsed galaxy of size: " + galaxyData.length);
+}
+else {
+    console.error("Can't find cached galaxy data....uhh, killing server I guess....");
+    server.close(() => {
+        process.exit(0);
+    });
+}
